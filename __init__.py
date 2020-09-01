8211f279e5c8e64e0984dc20e0e317dd659741ab
giftzwerg02 <chessplayer1@gmx.at>
2020-09-01 22:14:23 +0200

    ok

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import pandas as pd
+from   selenium import webdriver
+from   bs4      import BeautifulSoup
+import pandas   as pd
 
 def scraper():
     link = 'none'
